905d6425f3713001261c4bc09f31fdea16836157
Yusufola157 <yusufolaseni602@gmail.com>
2023-04-05 06:59:26 +0100

    updated

---
+++ src/components/blocks/homePageBlock/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = () => {
+  return (
+    <Cont>header</Cont>
+  )
+}
+export default Header
+const Cont = styled.div`
+position: fixed;
+
+width: 250px;
+height:100vh;
+background-color: red;
+`
